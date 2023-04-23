6294f3be65dd26b206e1ad18ba1249328bf0d7b3
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2023-04-23 17:14:40 -0400

    implementado envío de texto

---
+++ sender.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+
+BASE_URL = "https://web.whatsapp.com/"
+CHAT_URL = "https://web.whatsapp.com/send?phone={phone}&text&type=phone_number&app_absent=1"
+
+phone = 56900000000
+message = "test"
+
+options = Options()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://web.whatsapp.com/")
+
+driver.get(CHAT_URL.format(phone=phone))
+time.sleep(3)
+
+# chat text box location
+inp_xpath = (
+    '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
+)
+input_box = WebDriverWait(driver, 60).until(
+    expected_conditions.presence_of_element_located((By.XPATH, inp_xpath))
+)
+
+for i in range(20):
+    input_box.send_keys(message)
+    input_box.send_keys(Keys.ENTER)
+
+
+
